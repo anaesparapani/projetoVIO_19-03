2f1924bdc49ae4dd20149523e901db502a0fa2d7
anaesparapani <anaclaraesparapani@gmail.com>
2025-03-26 09:26:54 -0300

    26-03

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
+import DefaultLayout from "./DefaultLayout";
 
 const ProtectedRoute = ({children}) =>{
     const isAuthenticated = localStorage.getItem("authenticated");
-    return isAuthenticated ? children : <Navigate to="/" />; // se o authenticated for true ele mostra children, se for false volta para login
-}
+    return (isAuthenticated ? <DefaultLayout>{children}</DefaultLayout> : <Navigate to="/" />); // se o authenticated for true ele mostra children, se for false volta para login
+};
 export default ProtectedRoute;
